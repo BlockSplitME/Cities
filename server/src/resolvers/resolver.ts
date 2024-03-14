43a2be7a4d97fbd46684561c91854e0cc101ac10
BlockSplitME <evgeniy.belov.12@mail.ru>
2024-03-14 16:36:05 +0300

    status update

--- server/src/resolvers/resolver.ts
+++ server/src/resolvers/resolver.ts
@@ -56,14 +56,14 @@ export const getListOfCities = async(req, res) => {
 
 export const addListOfCities = async (req, res) => {
   const tableListsOfCities = AppDataSource.getMongoRepository(ListsOfCities);
-  const { name, fullname, color, cities } = req.body;
+  const { name, fullName, color, cities } = req.body;
   
   if (!name) {
     return res.status(400).json({ error: 'Получены некорректные данные' });
   }
   
   const hasItem = await tableListsOfCities.findOneBy({name: name})
-  const newList = tableListsOfCities.create({ name: name, fullName: fullname, color: color, cities: cities });
+  const newList = tableListsOfCities.create({ name: name, fullName: fullName, color: color, cities: cities });
   
   try {
     if(hasItem) res.status(400).json({ error: 'Объект уже сущестует.' + JSON.stringify(hasItem)});
@@ -78,21 +78,25 @@ export const addListOfCities = async (req, res) => {
 
 export const updateListOfCities = async (req, res) => {
   const tableListsOfCities = AppDataSource.getMongoRepository(ListsOfCities);
-  const { name, fullname, color, cities } = req.body;
+  const { name, fullName, color, cities } = req.body;
   const idList = req.params.id
   
   if (!name) {
     return res.status(400).json({ error: 'Получены некорректные данные' });
   }
+  const hasItem = await tableListsOfCities.findOneBy({name: name})
   
   try {
-    const newList = await tableListsOfCities.updateOne({_id: new ObjectId(idList)},{$set: {
-      name: name,
-      fullname: fullname,
-      color: color,
-      cities: cities
-    }})
-    res.json('Объект сохранен');
+    if(!hasItem) res.status(400).json({ error: 'Объекта не сущестует.'});
+    else {
+      const newList = await tableListsOfCities.updateOne({_id: new ObjectId(idList)},{$set: {
+        name: name,
+        fullName: fullName,
+        color: color,
+        cities: cities
+      }})
+      res.json('Объект сохранен');
+    }
   } catch (error) {
     res.status(500).json({ error: 'Ошибка сохранения данных' });
   }
